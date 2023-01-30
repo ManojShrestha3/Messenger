fcf83f98ed88637bafb9ee4371d1a4a57a48521c
manojshrestha <Manojshrestha3>
2023-01-30 19:25:17 +0900

    lesson-03

---
+++ file/SyainKanriLibrary/Syain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyainKanriLibrary
+{
+    public class Syain
+    {
+        public string SyainNo { get; set; }
+        public string SyainName { get; set; }
+        public DateTime NyusyaDate { get; set; }
+        public bool Zaisyoku { get; set; }
+
+    }
+}
---
+++ file/SyainKanriLibrary/SyainTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyainKanriLibrary
+{
+    public class SyainTable
+    {
+        public DataTable GetSyain()
+        {
+            DataTable table = new DataTable();
+
+            string ConnectionString = Properties.Settings.Default.DBConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM syain";
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+
+                adapter.Fill(table);
+            }
+        }
+       
+    }
+}
